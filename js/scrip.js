fe16027c6f7fe3a14623506c26fc302ac733284b
Andrey Ivanov <foresteraws@gmail.com>
2022-07-31 11:06:27 +0300

    Практика часть 2

--- js/scrip.js
+++ js/scrip.js
@@ -6,12 +6,49 @@ const personaMuvieDB = {
 	genres: {},
 	privat: false
 };
-const a = prompt('Один из просмотренніх фильмов?', ''),
-	b = prompt('На сколько оцените его', ''),
-	c = prompt('Один из последних просмотренніх фильмов?', ''),
-	d = prompt('На сколько оцените его', '');
 
-personaMuvieDB.muvies[a] = b;
-personaMuvieDB.muvies[c] = d;
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из просмотренніх фильмов?', ''),
+		b = prompt('На сколько оцените его', '');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personaMuvieDB.muvies[a] = b;
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+// let i = 0;
+// while (i < 2) {
+// 	const a = prompt('Один из просмотренніх фильмов?', ''),
+// 		b = prompt('На сколько оцените его', '');
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// 		personaMuvieDB.muvies[a] = b;
+// 	} else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// 	i++
+// }
+// let i = 0;
+// do {
+// 	const a = prompt('Один из просмотренніх фильмов?', ''),
+// 		b = prompt('На сколько оцените его', '');
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// 		personaMuvieDB.muvies[a] = b;
+// 	} else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// 	i++
+// } while (i < 2);
 
+if (personaMuvieDB.count < 10) {
+	console.log('Просмотренно довольно мало фильмов');
+} else if (personaMuvieDB.count >= 10 && personaMuvieDB.count < 30) {
+	console.log('Вы класический зритель');
+} else if (personaMuvieDB.count >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
 console.log(personaMuvieDB);
