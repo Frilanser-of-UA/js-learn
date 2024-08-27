4217456bfc7df02b993d54851f9ccd601ac83411
“Andrey <foresteraws@gmail.com>
2024-08-27 09:30:05 +0300

    Edit 27.08.24

--- js/scrip.js
+++ js/scrip.js
@@ -1,54 +1,12 @@
-const numberOFFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
-const personaMuvieDB = {
-	count: numberOFFilms,
-	muvies: {},
-	actors: {},
-	genres: {},
-	privat: false
-};
-
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из просмотренніх фильмов?', ''),
-		b = prompt('На сколько оцените его', '');
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-		personaMuvieDB.muvies[a] = b;
-	} else {
-		console.log('error');
-		i--;
+// Место для первой задачи
+function sayHello(name) {
+	console.log("Привет," + name);
+	return
+}
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+	for (let i = 5; i < 10; i++) {
+		console.log(i);
 	}
 }
-// let i = 0;
-// while (i < 2) {
-// 	const a = prompt('Один из просмотренніх фильмов?', ''),
-// 		b = prompt('На сколько оцените его', '');
-// 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-// 		personaMuvieDB.muvies[a] = b;
-// 	} else {
-// 		console.log('error');
-// 		i--;
-// 	}
-// 	i++
-// }
-// let i = 0;
-// do {
-// 	const a = prompt('Один из просмотренніх фильмов?', ''),
-// 		b = prompt('На сколько оцените его', '');
-// 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-// 		personaMuvieDB.muvies[a] = b;
-// 	} else {
-// 		console.log('error');
-// 		i--;
-// 	}
-// 	i++
-// } while (i < 2);
-
-if (personaMuvieDB.count < 10) {
-	console.log('Просмотренно довольно мало фильмов');
-} else if (personaMuvieDB.count >= 10 && personaMuvieDB.count < 30) {
-	console.log('Вы класический зритель');
-} else if (personaMuvieDB.count >= 30) {
-	console.log('Вы киноман');
-} else {
-	console.log('Произошла ошибка');
-}
-console.log(personaMuvieDB);
+returnNeighboringNumbers(5);
