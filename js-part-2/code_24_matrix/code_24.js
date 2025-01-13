f2fdb7d1299f9f26b28cce2b5e0dd10100093e24
“Andrey <foresteraws@gmail.com>
2025-01-13 10:40:38 +0200

    Added js-2 code_06 matrix, code_24 matrix

---
+++ js-part-2/code_24_matrix/code_24.js
@@ -0,0 +1,91 @@
+const arr = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
+
+console.log(arr);
+let step = 1;
+
+// рисую игровое поле
+for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < arr[i].length; k++) {
+        const div = document.createElement('div');
+        div.setAttribute('data-x', i);
+        div.setAttribute('data-y', k);
+        ttt.append(div);
+    }
+}
+
+function click(event) {
+    // получаю номер элемента по кот. кликнул
+    const x = +event.target.getAttribute('data-x');
+    const y = +event.target.getAttribute('data-y');
+    console.log(x, y);
+    // поле пустое?
+    if (arr[x][y] !== 0) return;
+    // Поле не занято - ставлю ход
+    arr[x][y] = step;
+
+    // визуализируем
+    draw();
+
+    // проверяем победу
+
+    checkWinner(step);
+
+    // меняем крестик-нолик
+    step = (step === 1) ? 2 : 1;
+}
+
+const tttDiv = document.querySelectorAll('#ttt>div');
+
+function draw() {
+    for (let i = 0; i < arr.length; i++) {
+        for (let k = 0; k < arr[i].length; k++) {
+            switch (arr[i][k]) {
+                case 1:
+                    tttDiv[i * 3 + k].textContent = 'X';
+                    break
+                case 2:
+                    tttDiv[i * 3 + k].textContent = '0';
+                    break
+            }
+        }
+    }
+}
+
+function checkWinner(step) {
+    if ((arr[0][0] === step && arr[0][1] === step && arr[0][2] === step)
+        || (arr[1][0] === step && arr[1][1] === step && arr[1][2] === step)
+        || (arr[2][0] === step && arr[2][1] === step && arr[2][2] === step)
+        || (arr[0][0] === step && arr[1][0] === step && arr[2][0] === step)
+        || (arr[0][1] === step && arr[1][1] === step && arr[2][1] === step)
+        || (arr[0][2] === step && arr[1][2] === step && arr[2][2] === step)
+        || (arr[0][0] === step && arr[1][1] === step && arr[2][2] === step)
+        || (arr[0][2] === step && arr[1][1] === step && arr[2][0] === step)) {
+        showWin(step);
+        return;
+    }
+    if (!arr.flat().includes(0)) showDraw();
+}
+
+function showWin(step) {
+    console.log('Победил ' + (step === 1 ? 'X' : '0'));
+    ttt.removeEventListener('click', click);
+    ttt.style.opacity = .5;
+    document.querySelector('.out').textContent = 'Победил: ' + (step === 1 ? 'X' : '0');
+}
+
+function showDraw() {
+    console.log('Draw');
+    ttt.removeEventListener('click', click);
+    ttt.style.opacity = .5;
+    document.querySelector('.out').textContent = 'Draw';
+}
+
+ttt.addEventListener('click', click);
+document.querySelector('.button-start-new-game').onclick = function () {
+    location.reload();
+}
